dccd354ca33d62952dfa5c2333d08761eed43a90
wellson99 <55526214+wellson99@users.noreply.github.com>
2019-10-29 21:48:14 +0800

    Added new sound effect
    - hit sound effect
    - miss sound effect

--- converted_battleship/src/GameResources.cs
+++ converted_battleship/src/GameResources.cs
@@ -49,10 +49,10 @@ public static class GameResources
 	private static void LoadSounds()
 	{
 		NewSound("Error", "error.wav");
-		NewSound("Hit", "hit.wav");
+		NewSound("Hit", "Explode.wav");
 		NewSound("Sink", "sink.wav");
 		NewSound("Siren", "siren.wav");
-		NewSound("Miss", "watershot.wav");
+		NewSound("Miss", "ShootMiss.wav");
 		NewSound("Winner", "winner.wav");
 		NewSound("Lose", "lose.wav");
 	}
